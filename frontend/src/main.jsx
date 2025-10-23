766e6b43abe235f9ded6bf87fdbb6921881034c3
CORA-HEAD <anmolfutela94@gmail.com>
2025-10-23 20:46:30 +0530

    delete vercel.json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter  } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import App from './App';
 import './index.css';
 import { ToastProvider } from "./contexts/ToastContext";
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter >
+  <BrowserRouter>
     <AuthProvider>
       <CartProvider>
           <ToastProvider>
@@ -15,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           </ToastProvider>
       </CartProvider>
     </AuthProvider>
-  </HashRouter >
+  </BrowserRouter>
 );
